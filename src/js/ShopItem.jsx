96b7823305ef23abacd26747528f051a7ed61653
Dmitriy Rovenskiy <arhitec@bk.ru>
2020-11-15 15:23:36 +0300

    style added v2.0

--- src/js/ShopItem.jsx
+++ src/js/ShopItem.jsx
@@ -6,12 +6,12 @@ export default function ShopItem() {
             <h2>Tiger of Sweden</h2>
             <h1>Leonard coat</h1>
             <h3>Minimalistic coat in cotton-blend</h3>
-            <div className={description}>
+            <div style={description}>
               Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.
             </div>
             <div className="highlight-window  mobile"><div className="highlight-overlay"></div></div>
             <div className="divider"></div>
-            <div className="purchase-info">
+            <div style={purchaseInfo}>
             <div className="price">£399.00</div>
               <button>Добавить в корзину</button>
             </div>  
@@ -19,12 +19,20 @@ export default function ShopItem() {
 }
 
 const mainContent = {
-  backgroundColor: 'blue',
-  color: '#fff',
-  margin: 'auto',
-  width: '400px'
+  backgroundColor: '#BEBEBE',
+  color: '#483D8B',
+  margin: '10px auto 0',
+  width: '400px',
+  padding: '5px',
+  border: '2px dashed #2F4F4F'
 };
 
 const description = {
   margineTOP: '2px',
+  marginBottom: '20px',
+};
+
+const purchaseInfo = {
+  display: 'flex',
+  justifyContent: 'space-between'
 }
